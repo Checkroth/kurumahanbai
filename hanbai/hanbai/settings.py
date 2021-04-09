698ff0e2a8ddff48d7c71859c696b29a90958eb5
checkroth <checkroth@gmail.com>
2021-04-09 17:30:58 +0900

    add support for db url

--- hanbai/hanbai/settings.py
+++ hanbai/hanbai/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,12 +79,15 @@ WSGI_APPLICATION = 'hanbai.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
+DATABASES = {}
+
+if os.getenv('DATABASE_URL'):
+    DATABASES['default'] = dj_database_url(os.getenv('DATABASE_URL'))
+else:
+    DATABASES['default'] = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
 
 
 # Password validation
